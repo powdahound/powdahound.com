2172b3e9a94a9069ea49640e4dd5d854c38ec960
Garret Heaton <powdahound@gmail.com>
2015-01-13 20:24:36 -0800

    Open external URLs in new tab

---
+++ public/js/powdahound.js
@@ -0,0 +1,7 @@
+// open external urls in new tab
+var links = document.links;
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+  if (links[i].hostname != window.location.hostname) {
+    links[i].target = '_blank';
+  }
+}
